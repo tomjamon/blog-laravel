7d28ead4e1fb3f989fb3658b017a38cc3cfc62ff
Tom Jamon <contact@tomjamon.com>
2020-05-01 16:00:39 +0200

    [UPD] Nettoyage de la page d'accueil

--- resources/views/frontend/posts/show.blade.php
+++ resources/views/frontend/posts/show.blade.php
@@ -68,6 +68,8 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
+                    <a href="{{ route('front.index') }}">Accueil</a>
+
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
@@ -82,22 +84,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{ $post->title }}
                 </div>
-
-                @foreach ($posts as $post)
-                    {{ $post->title }} <br>
-                @endforeach
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class=" m-b-md">
+                    {{ $post->content }}
                 </div>
             </div>
         </div>
