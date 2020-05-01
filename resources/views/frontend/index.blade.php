2bc828b9f834a334523d35d820d108e12763e640
Tom Jamon <contact@tomjamon.com>
2020-05-01 16:02:34 +0200

    [UPD] Nettoyage de la page d'accueil du blog

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -79,26 +79,15 @@
                     @endauth
                 </div>
             @endif
-
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
                 @foreach ($posts as $post)
-                    {{ $post->title }} <br>
+                    <a href="{{ route('front.posts.show', $post->slug) }}">
+                        {{ $post->title }}
+                    </a><br>
                 @endforeach
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
             </div>
         </div>
     </body>
