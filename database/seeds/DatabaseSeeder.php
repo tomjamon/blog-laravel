700b7d5324f5a37cd73ef33b3eb78042cf837d04
Tom Jamon <contact@tomjamon.com>
2020-05-29 16:23:01 +0200

    [UPD] Update seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $user = new \App\User();
+        $user->name = 'Tom Jamon';
+        $user->email = 'contact@tomjamon.com';
+        $user->password = \Illuminate\Support\Facades\Hash::make('contact@tomjamon.com');
+        $user->save();
+
+        for ($i = 0; $i<4; $i++) {
+            $post = new \App\Post();
+            $post->title = 'Mon article '.$i;
+            $post->slug = 'mon-article-'.$i;
+            $post->user_id = $user->id;
+            $post->save();
+        }
         // $this->call(UserSeeder::class);
     }
 }
